8c0a5f213a3ad1dc580f44852a701b8f12dd27a1
julinbestina <julin.bestina@yahoo.com>
2022-07-11 22:18:14 +0530

    Added code for unit conversion

---
+++ unitConversion.js
@@ -0,0 +1,11 @@
+let feet = 1 / 12 ;
+console.log("42 inch = " + 42 * feet + " feet");
+
+let meter = 0.3048;
+let area = 60 * 40;
+console.log("Area of Rectangular plot in feet = " + area + " feet")
+console.log("Area of Rectangular plot in meter = " + area * meter + " meter")
+
+let plotArea = area * 25;
+console.log("Area of 25 Rectangular plot in feet = " + plotArea + " feet")
+console.log("Area of 25 Rectangular plot in meter = " + plotArea * meter + " meter")
