f381d7013cb8d0d4a094e135ca762ba8d6682f1e
julinbestina <julin.bestina@yahoo.com>
2022-07-11 21:41:08 +0530

    Added code to Print sum of two dice values

---
+++ addDiceNumber.js
@@ -0,0 +1,3 @@
+let number1 = Math.floor(Math.random() *10) % 6 + 1;
+let number2 = Math.floor(Math.random() *10) % 6 + 1;
+console.log("Addition Result of " + number1 + " and " + number2 + " is " + (number1 + number2) );
