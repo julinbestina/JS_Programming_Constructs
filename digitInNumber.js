b3a1b43c81b0473ac37bd876846d95f805d8bdb4
julinbestina <julin.bestina@yahoo.com>
2022-07-16 12:35:06 +0530

    Added code to Print single digit number in words

---
+++ digitInNumber.js
@@ -0,0 +1,26 @@
+const prompt = require("prompt-sync")();
+
+let digit = prompt("Enter a single digit number: ");
+
+if(digit == 0)
+console.log("The digit is Zero");
+else if(digit == 1)
+console.log("The digit is One");
+else if(digit == 2)
+console.log("The digit is Two");
+else if(digit == 3)
+console.log("The digit is Three");
+else if(digit == 4)
+console.log("The digit is Four");
+else if(digit == 5)
+console.log("The digit is Five");
+else if(digit == 6)
+console.log("The digit is Six");
+else if(digit == 7)
+console.log("The digit is Seven");
+else if(digit == 8)
+console.log("The digit is Eight");
+else if(digit == 9)
+console.log("The digit is Nine");
+else 
+console.log("The digit is not a single digit number");
